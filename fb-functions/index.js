6cc229c9303db9b9a97d93c8b69c1213825a84b2
skunshd <skunshd@gmail.com>
2018-07-01 23:40:25 +0300

    add new email

--- fb-functions/index.js
+++ fb-functions/index.js
@@ -12,8 +12,8 @@ var cors = require('cors')({
 // 1. https://www.google.com/settings/security/lesssecureapps
 // 2. https://accounts.google.com/DisplayUnlockCaptcha
 // For other types of transports such as Sendgrid see https://nodemailer.com/transports/
-var gmailEmail = 'skunshd@gmail.com';
-var gmailPassword = 'mivina777%)';
+var gmailEmail = 'chess.lessons.kiev@gmail.com';
+var gmailPassword = 'j2oox892';
 
 var mailTransport = nodemailer.createTransport({
     service: 'gmail',
@@ -26,24 +26,24 @@ var mailTransport = nodemailer.createTransport({
 function sendWelcomeEmail(messageData) {
     var mailOptions = {
         from: '<noreply@chess-lessons-kiev.firebaseapp.com>',
-        to: 'maliutenko.d@gmail.com'
+        to: 'verachern@mail.ru'
     };
 
-    mailOptions.subject = 'Email from visitor ' + messageData.contact;
+    mailOptions.subject = 'Сообщение от:' + messageData.contact;
     mailOptions.text = messageData.message;
 
     return mailTransport.sendMail(mailOptions).then(function () {
-        console.log('New email sent ok!');
+        // console.log('New email sent ok!');
+    }).catch(function (err) {
+        console.log('mailTransport err!', err);
     });
 }
 
 
 // functions
 exports.sendemail = functions.https.onRequest(function (req, res) {
-
     cors(req, res, function() {
         sendWelcomeEmail(req.body);
-        console.log("----|> req.body", req.body)
         res.status(200).send(req.body);
     });
 });
